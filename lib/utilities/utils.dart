ce444e7d0804fbbac92c301191693e88de5dd695
Dannel Lim Khye Hsen <dannel_lim@hotmail.com>
2023-04-22 13:57:31 +0800

    Update utils.dart
    to accodmodate .it, .ch, 2 letter email endings

--- lib/utilities/utils.dart
+++ lib/utilities/utils.dart
@@ -1,7 +1,7 @@
 class Utils {
   static bool isEmailValid(String email) {
     return RegExp(
-            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]{3,}")
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}")
         .hasMatch(email);
   }
 
